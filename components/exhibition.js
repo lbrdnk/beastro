d089077691465596bf73fdc701239a286be153ef
Libor Danek <lbr.dnk@posteo.net>
2021-12-13 14:36:34 +0100

    Checkpoint: exhibitions mobile + desktop

--- components/exhibition.js
+++ components/exhibition.js
@@ -3,72 +3,98 @@ import Image from "next/image";
 
 const baseUrl = "http://localhost:1337"
 
+// potrebujem toto -- nestaci mi next/image fill mode? asi ne
+function getBoxFittingDimensions(availWidth, availHeight, imageWidth, imageHeight) {
+
+    const coef = Math.max(imageWidth / availWidth, imageHeight / availHeight);
+    // sem clamp min
+    const width = parseInt(Math.ceil(imageWidth / coef));
+    const height = parseInt(Math.ceil(imageHeight / coef));
+
+    // console.log(availWidth, availHeight, imageWidth, imageHeight, width, height)
+
+    return { width, height }
+}
+
+function useWindowDimensions() {
+
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        const updateWindowDimensions = () => {
+            const { innerWidth: width, innerHeight: height } = window;
+            setDimensions({ width, height })
+        };
+        updateWindowDimensions()
+        window.addEventListener("resize", updateWindowDimensions)
+        return () => window.addEventListener("resize", updateWindowDimensions)
+    }, [])
+
+    console.log("dim", dimensions)
+
+    return dimensions;
+}
+
 // 1. check proportions of invitation
 export default function Exhibition({ e }) {
 
-    const ref = useRef(null);
-    const [dimensions, setDimensions] = useState([]);
+    const { width: innerWidth, height: innerHeight } = useWindowDimensions()
 
-    // TODO check if must be here, if im not able to use ref val directly
-    useEffect(() => {
-        if (ref && ref.current) {
-            // console.log(ref)
-            setDimensions([ref.current.offsetWidth, ref.current.offsetHeight])
-        }
-    }, [ref])
-
-    // console.log(e.invitation)
-
-    // ! TODO natvrdo vypocitat exhibition width / height
-    // TODO ref or state
-    let usableBox = useRef([0, 0]);
-    useEffect(() => {
-            
-        usableBox.current = [window.innerWidth, window.innerHeight];
-        
-            // return usableBox
-            // console.log(window.innerWidth)
-            // console.log(window.innerHeight)
-    })
-
-    // height max = usableBox[1] - <height of visible header part>
-    // width  max = md-screen || full screen
-
-    // TODO refactor
     const headerSize = 80;
-    const maxInvBoxHeight = usableBox.current[1] - headerSize;
-    const maxInvBoxWidth = usableBox.current[0];
-    console.log([maxInvBoxHeight, maxInvBoxWidth])
 
+    let imageUsedWidth, imageUsedHeight;
+    if (e.invitation) {
+        const { width: imageOrigWidth, height: imageOrigHeight } = e.invitation;
+        ({
+            width: imageUsedWidth,
+            height: imageUsedHeight
+        } = getBoxFittingDimensions(innerWidth, innerHeight - headerSize, imageOrigWidth, imageOrigHeight));
+    }
 
-    // fill it with pic -- responsive?
-
+    // console.log(image)
 
     return (
+        // container
         <div className="flex flex-col justify-center items-center">
 
-            {/* photos */}
+            {/* photos container*/}
             <div
+                // p-4 removed
                 className={
-                    "flex flex-col w-full justify-center space-y-4 p-4"
+                    "flex flex-col w-full justify-center items-center space-y-4"
                     + (e.invitation ? " before:h-[calc(100vh-80px)] before:mt-20" : "")
                 }
             >
-
+                {/* toto sa neupdatne pri zmene velkosti okna? */}
                 {e.photos.map(({ width, height, url }) => {
+                    console.log("update")
+                    const availWidth = innerWidth;
+                    const availHeight = innerHeight - headerSize;
+                    const {
+                        width: imageUsedWidth,
+                        height: imageUsedHeight
+                    } = getBoxFittingDimensions(availWidth, availHeight, width, height);
+
                     return (
 
-                        // photo frame -- shadow
-                        //                        <div key={url} className="z-20 bg-white w-full p-2 shadow-2xl">
+                        // photo frame -- margin -- 
+                        <div key={url} className="w-full z-20 p-4 md:p-8"
+                            style={{
+                                width: imageUsedWidth + "px",
+                                height: imageUsedHeight + "px",
+                            }}>
 
-                        <div key={url} className="w-full z-20 relative shadow-2xl p-2 bg-white">
-                            <Image
-                                src={baseUrl + url}
-                                width={parseInt(width)}
-                                height={parseInt(height)}
-                                layout="responsive"
-                                sources={"100vw"}
-                            />
+                            <div key={url} className="relative z-20 shadow-2xl p-2 bg-white"
+
+                            >
+                                <Image
+                                    src={baseUrl + url}
+                                    width={parseInt(width)}
+                                    height={parseInt(height)}
+                                    layout="responsive"
+                                    sources={"100vw"}
+                                />
+                            </div>
                         </div>
                     );
                 })}
@@ -76,18 +102,15 @@ export default function Exhibition({ e }) {
             {/* photos end */}
 
             {e.invitation && (
-                <div className="sticky w-full flex justify-center items-center"
+                <div className={`X${imageUsedWidth} Y${imageUsedHeight} sticky w-full flex justify-center items-center`}
                     style={{
-                        // TODO pridat clamp aby to bolo od hora max kusok
-                        bottom: `calc(calc(100vh - 80px) / 2 - ${dimensions[1] / 2}px)`,
-
-                        height: maxInvBoxHeight + "px",
-                        width: maxInvBoxHeight + "px"
+                        bottom: `calc(calc(100vh - 80px) / 2 - ${imageUsedHeight / 2}px)`,
+                        width: imageUsedWidth + "px",
+                        height: imageUsedHeight + "px"
                     }}
                 >
                     <div
-                        ref={ref}
-                        className="sticky bottom-0 w-full"
+                        className="sticky bottom-0 w-full h-full"
                         style={{
                             // bottom: "0px"
                         }}
@@ -103,7 +126,7 @@ export default function Exhibition({ e }) {
                 </div>
             )}
             {/* popisok */}
-            <div>{e.description}</div>
+            {/* <div>{e.description}</div> */}
 
         </div>
     )
