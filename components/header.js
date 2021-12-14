a4abafe085a49635b6bf57043f325b91147dfc7a
Libor Danek <lbr.dnk@posteo.net>
2021-12-14 14:56:59 +0100

    Checkpoint: magic first iteration done (mobile + desktop), Remove expensive fonts

---
+++ components/header.js
@@ -0,0 +1,174 @@
+import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import beastroType from '../public_tmp/img/beastro_type.jpg';
+import beastroSquare from '../public_tmp/img/beastro_square.jpg';
+
+const menuItems = [
+    {
+        path: "/exhibitions",
+        title: "Exhibitions"
+    },
+    {
+        path: "/magic",
+        title: "Magic"
+    }
+];
+
+// args should be height, upper height
+export default function Header() {
+        
+    const menuRef = useRef(null);
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    // fix height for square logo
+    const sqaureHeight = 64;
+    const squareWidth = parseInt(sqaureHeight * beastroSquare.width / beastroSquare.height);
+
+    // fix height for rect logo
+    const btHeight = 64;
+    const btWidth = parseInt(btHeight * beastroType.width / beastroType.height);
+
+    // out click when menu opened handling
+    useEffect(() => {
+        const clickHandler = (e) => {
+            if (!menuRef.current.contains(e.target)) {
+                setMenuOpened(!menuOpened)
+            }
+        }
+        if (menuOpened) {
+            document.addEventListener("click", clickHandler);
+            return () => { document.removeEventListener("click", clickHandler) };
+        }
+    }, [menuOpened])
+
+    return (
+        <div
+            ref={menuRef}
+            className={
+                "fixed z-40 w-full h-60 transition-all duration-700 shadow-lg backdrop-blur-md"
+                + " "
+                + (menuOpened ? "top-0 bg-black/10" : "-top-40 bg-white/30")
+            }
+        >
+            {/* header start */}
+
+            {/* header upper start */}
+            <div className="flex flex-row justify-start h-40">
+
+                {/* NAV div */}
+                <div className=" flex flex-col justify-between p-1 items-end h-full"
+                    style={{
+                        minWidth: "calc(50% - 40px)"
+                    }}
+                >
+
+                    <Link href="/">
+                        {/* beastroType img link */}
+                        <div className="shadow-lg m-1 cursor-pointer"
+                            style={{
+                                width: parseInt(btWidth) + "px",
+                                height: parseInt(btHeight) + "px",
+                            }}
+                            onClick={() => {
+                                setMenuOpened(!menuOpened);
+                            }}
+                        >
+                            <div className="relative w-full h-full"
+
+                            >
+                                <Image
+                                    src={beastroType}
+                                    layout="fill"
+                                    objectFit="cover"
+                                    priority
+                                />
+                            </div>
+                        </div>
+                    </Link>
+                    {/* beastroType img link end */}
+                    {/* other nav links */}
+                    {
+                        menuItems.map(({ path, title }) => {
+                            return (
+                                // link functions created with 
+                                <Link key={title} href={path}>
+                                    <a className={"bg-white m-1 shadow-lg pl-0.5"}
+                                        onClick={() => {
+                                            setMenuOpened(!menuOpened)
+                                        }}
+                                        style={{
+                                            // fontFamily: "'Kosugi', sans-serif",
+                                            fontSize: "1rem",
+                                            letterSpacing: "2px",
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {title}
+                                    </a>
+                                </Link>
+                            );
+                        })
+                    }
+                    {/* other nav links end */}
+                </div>
+                {/* ABOUT div */}
+                <div className="p-1 m-1 flex-grow flex justify-center items-stretch max-w-xs">
+                    <div
+                        className="w-full flex flex-col justify-center items-center bg-white shadow-lg"
+                    >
+                        {
+                            ["Gallery.", "Mikulášska 27,", "Bratislava."].map((line) => {
+                                return (
+                                    <p key={line}
+                                        className="pl-2 text-center"
+                                        style={{
+                                            // fontFamily: "'Kosugi', sans-serif",
+                                            fontSize: "1rem",
+                                            letterSpacing: "2px",
+                                        }}
+                                    >
+                                        {line}
+                                    </p>
+
+                                );
+                            })
+                        }
+
+                    </div>
+                </div>
+                {/* ABOUT div end */}
+            </div>
+            {/* header upper end */}
+
+            {/* header visible start */}
+            <div className="relative flex justify-center items-center h-20">
+                <div className="rounded-full shadow-lg"
+                    style={{
+                        width: "64px",
+                        height: "64px",
+                        overflow: "hidden",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <div className="cursor-pointer relative shadow-lg"
+                        onClick={() => setMenuOpened(!menuOpened)}
+                        style={{ width: squareWidth, height: sqaureHeight }}
+                    >
+                        <Image
+                            src={beastroSquare}
+                            layout="fill"
+                            objectFit="contain"
+                            quality={100}
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* header visible end */}
+
+        </div>
+    )
+}
---
+++ pages/magic.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+import Image from "next/image";
+const baseUrl = "http://localhost:1337/moments"
+
+function getBoxFittingDimensions(availWidth, availHeight, imageWidth, imageHeight) {
+
+    const coef = Math.max(imageWidth / availWidth, imageHeight / availHeight);
+    // sem clamp min
+    const width = parseInt(Math.ceil(imageWidth / coef));
+    const height = parseInt(Math.ceil(imageHeight / coef));
+
+    // console.log(availWidth, availHeight, imageWidth, imageHeight, width, height)
+
+    return { width, height }
+}
+
+function useWindowDimensions() {
+
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        const updateWindowDimensions = () => {
+            const { innerWidth: width, innerHeight: height } = window;
+            setDimensions({ width, height })
+        };
+        updateWindowDimensions()
+        window.addEventListener("resize", updateWindowDimensions)
+        return () => window.addEventListener("resize", updateWindowDimensions)
+    }, [])
+
+    // console.log("dim", dimensions)
+
+    return dimensions;
+}
+
+export async function getStaticProps(context) {
+
+    const res = await fetch(baseUrl);
+    const data = await res.json()
+
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    // data.reverse();
+
+    return {
+        props: { data }, // will be passed to the page component as props
+    }
+}
+
+export default function Moments({ data }) {
+
+    const headerHeight = 80;
+
+    const moments = [...data]
+
+    const { width: innerWidth, height: innerHeight } = useWindowDimensions();
+
+    const availWidth = Math.min(innerWidth, 768);
+    const availHeight = innerHeight - headerHeight;
+
+
+
+
+    return (
+
+        // main container
+        <div className="mt-20 flex flex-row w-full justify-center items-center">
+
+            {/* photo container */}
+            <div className="flex flex-row flex-wrap w-full max-w-3xl justify-center items-center">
+
+                {data.map(({ photos }) => {
+                    photos.reverse();
+
+
+                    return photos.map((photo, index) => {
+
+                        const { width: imageOrigWidth, height: imageOrigHeight } = photo;
+                        const { width, height } = getBoxFittingDimensions(availWidth, availHeight, imageOrigWidth, imageOrigHeight);
+
+                        return (
+                            <div
+                                key={photo.id}
+                                className="p-4 "
+                                style={{
+                                    width: `${width}px`,
+                                    height: `${height}px`
+                                }}
+                            >
+                                <div
+                                    key={photo.id}
+                                    className="relative bg-white shadow-xl"
+                                    style={{
+                                        
+                                    }}
+                                >
+                                    <Image
+                                        src={"http://localhost:1337" + photo.url}
+                                        width={photo.width}
+                                        height={photo.height}
+                                        layout="responsive"
+                                        sizes={"100vw"}
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })
+                })}
+            </div>
+        </div>
+    );
+}
