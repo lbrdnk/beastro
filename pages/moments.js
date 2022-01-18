de02da3a4e583f59c8ccf08f8d553d16d4186f0c
Libor Danek <lbr.dnk@posteo.net>
2022-01-18 16:41:52 +0100

    Add lightbox capabilities to moments

--- pages/moments.js
+++ pages/moments.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import { getApiAccessToken, getBoxFittingDimensions, useWindowDimensions, groupByHeight } from "../lib/utils";
 
+
+import Lightbox from "../components/lightbox";
+
 export async function getStaticProps(context) {
 
     const token = await getApiAccessToken();
@@ -43,7 +46,7 @@ export default function Moments({
 
     const allPhotos = ms.map(m => [...m.photos]).flat()
 
-    
+
 
     // initialize null vs 0
     const [windowWidth, setWindowWidth] = useState(0);
@@ -60,50 +63,75 @@ export default function Moments({
 
     // console.log(windowWidth)
 
-    // no point counting photo columns when window size was not set
-    // -> no point rendering when have no photo columns
-    if (windowWidth === 0) {
-        return null
-    }
+
 
     let columnCount = 3;
     if (windowWidth >= 480) {
         columnCount = 5;
     }
 
+    // TODO: maybe it makes sense to be more specific with constrains here, performance wise
+    // let imageSize = windowWidth >= 480 ? "30vw" : "15vw";
+    let imageSize = windowWidth >= 480 ? "200px" : "30vw"; // this looks fine
+    // let imageSize = "200px";
+
     const gs = groupByHeight(allPhotos, columnCount).groups;
 
+    const imagesForLight = gs.reduce((group, acc) => [...acc, ...group], [])
+
+    const [isLightBoxOpened, setIsLightBoxOpened] = useState(false);
+    const closeLightbox = () => setIsLightBoxOpened(false);
+    const [lightboxImages, setLightboxImages] = useState(imagesForLight);
+    const [selectedLightboxImageId, setSelectedLightboxImageId] = useState(null);
+    // TODO how to recreate images list to scroll line by line?
+    // currently scrolling by columns
+
+
+    // THIS MUST BE AFTER HOOKS
+    // no point counting photo columns when window size was not set
+    // -> no point rendering when have no photo columns
+    if (windowWidth === 0) {
+        return null
+    }
+
     return (
+        <>
+            {isLightBoxOpened && <Lightbox initialSelectedId={selectedLightboxImageId} images={lightboxImages} closeFn={closeLightbox} />}
 
-        // main container
-        <div className="flex flex-row w-full justify-center items-start p-4 ">
-            {/* photo container */}
-            <div className="flex flex-row w-full justify-center items-stretch max-w-3xl space-x-4">
+            <div className="flex flex-row w-full justify-center items-start p-4 ">
+                {/* photo container */}
+                <div className="flex flex-row w-full justify-center items-stretch max-w-3xl space-x-4">
 
-                {gs.map((colPhotoArr, idx) => {
-                    return (
-                        <div key={idx} className="flex flex-col flex-grow justify-between space-y-4">
-                            {colPhotoArr.map((photo, index) => (
-                                <div
-                                    key={photo.id}
-                                    className="relative shadow-lg"
-                                >
-                                    <Image
-                                        src={process.env.NEXT_PUBLIC_CMS_BASE_URL + photo.url}
-                                        width={photo.width}
-                                        height={photo.height}
-                                        layout="responsive"
-                                        sizes={"30vw"}
-                                        lazyBoundary="1400px"
-                                    />
-                                </div>
+                    {gs.map((colPhotoArr, idx) => {
+                        return (
+                            <div key={idx} className="flex flex-col flex-grow justify-between space-y-4">
+                                {colPhotoArr.map((photo, index) => (
+                                    <div
+                                        key={photo.id}
+                                        className="relative shadow-lg"
+                                        onClick={() => {
+                                            setSelectedLightboxImageId(photo.id);
+                                            setIsLightBoxOpened(true);
+                                        }}
+                                    >
+                                        <Image
+                                            src={process.env.NEXT_PUBLIC_CMS_BASE_URL + photo.url}
+                                            width={photo.width}
+                                            height={photo.height}
+                                            layout="responsive"
+                                            sizes={imageSize}
+                                            quality={50}
+                                        // lazyBoundary="1400px"
+                                        />
+                                    </div>
 
-                            ))}
-                        </div>
-                    )
-                })}
+                                ))}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+        </>
     );
 
 }
