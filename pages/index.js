cedd04219e4ab66ac9e417b36ff02e517fce18de
Libor Danek <lbr.dnk@posteo.net>
2022-01-22 16:31:11 +0100

    Fix filter on index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 // import hero from "../public/index_hero.jpg";
-import hero from "../public/index_hero_kaminsky_xp.jpg";
+import hero from "../public/index_hero_kaminsky_xp_2.jpg";
 
 export default function Home({  }) {
 
@@ -13,7 +13,8 @@ export default function Home({  }) {
         <div className="w-full flex flex-col items-center">
             <div className="w-full max-w-3xl">
                 <div className="w-full shadow-2xl p-2">
-                    <div className="w-full relative filter invert"
+                    {/* <div className="w-full relative filter invert" */}
+                    <div className="w-full relative"
                         style={{
                             height: height + "px",
                             // width: width + "px"
@@ -23,9 +24,9 @@ export default function Home({  }) {
                             src={hero}
                             layout="fill"
                             objectFit="cover"
-                            // objectPosition="-530px 0%"
+                            // objectPosition="20% 0%"
                             priority
-                            quality={50}
+                            quality={100}
                             // className='filter blur-md'
                         />
                     </div>
