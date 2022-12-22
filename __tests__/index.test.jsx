ace9e5ffb2ee372ab023ec40f02399c8821e498a
Libor Danek <lbr.dnk@posteo.net>
2022-12-22 22:30:08 +0100

    First jest index test mock

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,45 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
+
+const imgUrl =
+  "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png";
+
+describe("Home", () => {
+  it("renders a heading", async () => {
+    const { container } = render(
+      <Home
+        indexFlyer={{
+          width: 848,
+          height: 636,
+          url: imgUrl,
+        }}
+        // imgProps={{priority: true}}
+      />
+    );
+
+    await waitFor(() => {
+      expect(screen.getByRole("img")).toHaveAttribute(
+        "src",
+        expect.stringContaining(encodeURIComponent(imgUrl))
+      );
+    });
+
+    //
+    // expect(1).toBe(1)
+    // const image = document.querySelectorAll("img");
+    // // console.log(Object.keys(image))
+
+    // console.log(container.querySelector("img"));
+    // console.log(screen.getByRole("img", { src: url }));
+    // console.log(
+    //   decodeURIComponent(screen.getByRole("img", { src: imgUrl }).src)
+    // );
+
+    // const heading = screen.getByRole('heading', {
+    //   name: /welcome to next\.js!/i,
+    // })
+
+    // expect(heading).toBeInTheDocument()
+  });
+});
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     // TODO add contentful
-    domains: ['localhost', 'cms.beastro.sk', "images.ctfassets.net"],
+    domains: ['localhost', 'beastro.sk', "upload.wikimedia.org", "images.ctfassets.net"],
     deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
